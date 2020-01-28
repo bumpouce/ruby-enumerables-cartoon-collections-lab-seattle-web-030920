9be2d65e3b67b6ddc2729bbc8ea4c3746bcfe99c
Christine Bumpous <bumpouce@gmail.com>
2020-01-28 20:48:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,15 @@
 def roll_call_dwarves(array)
-
   array.each_with_index do |dwarf, index|
     pp "#{index+1}. #{dwarf}"
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  updated_array = []
+  
+  
+  
+  updated_array
 end
 
 def long_planeteer_calls# code an argument here
